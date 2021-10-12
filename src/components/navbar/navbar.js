1a0470af7e860cf992e31fc677f3a23d6a71806a
Rashidul Hasan <rhratul12@gmail.com>
2021-10-12 18:31:03 +0600

    update toggle

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from '../../assets/img/navlogo.png'
 import { Link } from 'react-router-dom'
 
 
 function Navbar() {
+    let icon = 'bi bi-list mobile-nav-toggle'; 
+    // 'bi mobile-nav-toggle bi-x'
+    const [isClicked,  setClick] = useState(false);
+    const handleClick=()=>{
+        isClicked?  setClick(!isClicked): setClick(!isClicked);
+    }
     return (
         <header id="header" className="fixed-top">
             <div className="container d-flex align-items-center justify-content-between">
                 <h1 className="logo"><Link to="/"><img src={logo} alt="" /></Link></h1>
 
-                <nav id="navbar" className="navbar">
+                <nav id="navbar" className={isClicked?['navbar','navbar-mobile'].join(' '):"navbar"}>
                     <ul>
                         <li className="active"><Link className="nav-link scrollto active" to="/">Home</Link></li>
                         <li><Link className="nav-link scrollto" to={{ pathname: '/', hash: '#services' }}>Services</Link></li>
@@ -26,7 +32,7 @@ function Navbar() {
                         <li><a className="nav-link scrollto" href="/#contact">Contact</a></li>
                         <li><Link className="getstarted scrollto" to='/career' >Career</Link></li>
                     </ul>
-                    <i className="bi bi-list mobile-nav-toggle" />
+                    <i className={isClicked? 'bi mobile-nav-toggle bi-x': 'bi bi-list mobile-nav-toggle'} onClick={()=>handleClick()} />
                 </nav>{/* .navbar */}
             </div>
         </header>
